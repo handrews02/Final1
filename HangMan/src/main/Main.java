883b06e71f1994965fe29db18a9884e9cd81cbd8
Hannah Andrews <handrews0214@gmail.com>
2018-10-10 09:46:00 -0500

    updated text

--- HangMan/src/main/Main.java
+++ HangMan/src/main/Main.java
@@ -3,24 +3,34 @@ package main;
 import java.util.Scanner;
 import java.util.Random;
 
-public class Main 
-{
 
-	public static void main(String[] args) 
-	{
+public class Main {
+	private static String[] words = {"courageous", "magnificent", "terrible", "righteous", "superficial" };
+	private static String word = words[(int) (Math.random() * words.length)];
+	private static String replace = new String(new char[word.length()]).replace("\0", "*");
+	private static int attempts = 0;
+
+public static void main(String[] args) {
 		
-		WordGenerator.displayWords();
+		Scanner input = new Scanner(System.in);
 		
-	}
+		while (attempts < 7 && replace.contains("*")) {
+			System.out.println("Guess a letter in the word>> ");
+			System.out.println(replace);
+			String guess = input.next();
+			game(guess);}
+		
+		
+}
+public static void game(String guess) {
+	String newReplace = "";
+	for (int i = 0; i < word.length(); i++) {
+			if(guess == word) {
+				newReplace += guess;
+			}
+		
+		
+		}
 	
-	public static void displayWord1()
-	{
-		char[] word = null;
-		System.out.print(word);
-		
-		
-		
-		
 	}
-	
 }
