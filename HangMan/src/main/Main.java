b9b3582ee1c0dc5837885f26d5d98012e90adeae
Hannah Andrews <handrews0214@gmail.com>
2018-10-12 09:48:29 -0500

    update

--- HangMan/src/main/Main.java
+++ HangMan/src/main/Main.java
@@ -1,134 +1,48 @@
-//package main;
-//
-//import java.util.Scanner;
-//import java.util.Random;
-//
-//
-//
-//
-//public class Main {
-//
-//	private static String[] words = {"terminator", "banana", "computer", "cow", "rain", "water" };
-//	private static String word = words[(int) (Math.random() * words.length)];
-//	private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
-//	private static int count = 0;
-//
-//	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//
-//		while (count < 7 && asterisk.contains("*")) {
-//			System.out.println("Guess any letter in the word");
-//			System.out.println(asterisk);
-//			String guess = sc.next();
-//			hang(guess);
-//		}
-//		sc.close();
-//	}
-//
-//	public static void hang(String guess) {
-//		String newasterisk = "";
-//		for (int i = 0; i < word.length(); i++) {
-//			if (word.charAt(i) == guess.charAt(0)) {
-//				newasterisk += guess.charAt(0);
-//			} else if (asterisk.charAt(i) != '*') {
-//				newasterisk += word.charAt(i);
-//			} else {
-//				newasterisk += "*";
-//			}
-//		}
-//
-//		if (asterisk.equals(newasterisk)) {
-//			count++;
-//			hangmanImage();
-//		} else {
-//			asterisk = newasterisk;
-//		}
-//		if (asterisk.equals(word)) {
-//			System.out.println("Correct! You win! The word was " + word);
-//		}
-//	}
-//
-//	public static void hangmanImage() {
-//		if (count == 1) {
-//			System.out.println("Wrong guess, try again");
-//			System.out.println();
-//			System.out.println();
-//			System.out.println();
-//			System.out.println();
-//			System.out.println("___|___");
-//			System.out.println();
-//		}
-//		if (count == 2) {
-//			System.out.println("Wrong guess, try again");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("___|___");
-//		}
-//		if (count == 3) {
-//			System.out.println("Wrong guess, try again");
-//			System.out.println("   ____________");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("   | ");
-//			System.out.println("___|___");
-//		}
-//		if (count == 4) {
-//			System.out.println("Wrong guess, try again");
-//			System.out.println("   ____________");
-//			System.out.println("   |          _|_");
-//			System.out.println("   |         /   \\");
-//			System.out.println("   |        |     |");
-//			System.out.println("   |         \\_ _/");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("   |");
-//			System.out.println("___|___");
-//		}
-//		if (count == 5) {
-//			System.out.println("Wrong guess, try again");
-//			System.out.println("   ____________");
-//			System.out.println("   |          _|_");
-//			System.out.println("   |         /   \\");
-//			System.out.println("   |        |     |");
-//			System.out.println("   |         \\_ _/");
-//			System.out.println("   |           |");
-//			System.out.println("   |           |");
-//			System.out.println("   |");
-//			System.out.println("___|___");
-//		}
-//		if (count == 6) {
-//			System.out.println("Wrong guess, try again");
-//			System.out.println("   ____________");
-//			System.out.println("   |          _|_");
-//			System.out.println("   |         /   \\");
-//			System.out.println("   |        |     |");
-//			System.out.println("   |         \\_ _/");
-//			System.out.println("   |           |");
-//			System.out.println("   |           |");
-//			System.out.println("   |          / \\ ");
-//			System.out.println("___|___      /   \\");
-//		}
-//		if (count == 7) {
-//			System.out.println("GAME OVER!");
-//			System.out.println("   ____________");
-//			System.out.println("   |          _|_");
-//			System.out.println("   |         /   \\");
-//			System.out.println("   |        |     |");
-//			System.out.println("   |         \\_ _/");
-//			System.out.println("   |          _|_");
-//			System.out.println("   |         / | \\");
-//			System.out.println("   |          / \\ ");
-//			System.out.println("___|___      /   \\");
-//			System.out.println("GAME OVER! The word was " + word);
-//		}
-//	}
-//}
+package main;
+import java.util.Scanner;
+
+    public class Main {
+
+public static void main(String[] args) {
+        char player='o';
+        int row,column;
+        Scanner k =  new Scanner (System.in);
+        Game g = new Game ();
+
+        g.initializeGame();
+        System.out.println("Game ready !\n");
+
+while(true)
+{   
+player=g.changePlayer(player);
+System.out.print("\n"+player +" ,choose your location (row, column):");
+row=k.nextInt();
+column=k.nextInt();
+
+    while (g.checkIfLegal(row,column))
+    {
+    System.out.println("That slot is already taken or out of bounds, please try again (row, column).");
+    g.displayBoard();
+    row=k.nextInt();
+    column=k.nextInt();
+    }
+g.changeBoard(player,row,column );
+g.displayBoard();
+
+if(g.checkIfWinner())
+    {
+    System.out.println("\nThe winner is "+ player+" !");
+    break;
+
+    }
+if(g.checkIfTie())
+    {
+        System.out.println("\nThe game is a tie !");
+        break;
+    }
+}
+
+
+    }
+
+    }
